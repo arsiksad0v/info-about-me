30ec05e3076db7fb6faa7d9e489755450dbb94d3
Arstan <arsiksad0v.wgod@gmail.com>
2024-06-25 17:18:57 +0600

    styles improved

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -5,7 +5,7 @@ const About: React.FC = () => {
     <div>
       <h1>About Me</h1>
       <p>This page contains information about me.</p>
-      <p>Steam Profile: <a href="https://steamcommunity.com/id/arsiksad0vvvvvvvvvvvvvvvvvv/">Arsik</a></p>
+      <p>My name is Arstan. I am 18 years old. Recently i have opened an opportunity for myself how to do nothing and be a wealthy bastard called it IT.</p>
     </div>
   );
 };
--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -5,6 +5,8 @@ const Contacts: React.FC = () => {
     <div>
       <h1>Contacts</h1>
       <p>Here you can find my contact information.</p>
+      <p>Telegram: @arsiksad0v</p>
+      <p>Phone: +996222910106</p>
     </div>
   );
 };
